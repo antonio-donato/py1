ba3f91ac16ca7246f5a9f43b7d69fff15e585043
Antonio Donato <antonio.donato@cgm.com>
2021-03-16 18:38:21 +0100

    aggiunti altri comandi

--- test.py
+++ test.py
@@ -2,7 +2,8 @@
 file_csv = open("C:\\Users\\Antonio\\Downloads\\contacts1.csv", "r")
 
 #open the file in write mode (create and overwrite)
-file_new = open("C:\\Users\\Antonio\\Downloads\\test_py.csv", "w")
+file_new_nome_file = "C:\\Users\\Antonio\\Downloads\\test_py.csv"
+file_new = open(file_new_nome_file, "w")
 
 #split the red record into the element of the list for each comma divider
 contenuto = file_csv.read().split(",")
@@ -14,10 +15,26 @@ for x in contenuto:
    if x:
       new_contenuto.append(x)
       print(x)
-
 file_new.writelines(["%s\n" % item for item in new_contenuto])
+
+#print(testo.format(uno, due))
 
 print("--------------------")
 
+#Use format
+uno = 1
+due = "due"
+testo = "questa una riga con dei parametri di test {0} {1}"
+file_new.writelines(testo.format(uno, due))
+
 file_csv.close()
 file_new.close()
+
+#Read file
+file_open = open(file_new_nome_file, "r")
+print("====================")
+print(file_open.readline())
+print("====================")
+file_open.close()
+
+print(testo.find("questa"))
