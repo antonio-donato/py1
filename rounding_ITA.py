3b837516ecc2dce80d51399e07d7123708775359
Antonio Donato <antonio.donato@cgm.com>
2021-03-31 17:59:08 +0200

    rounding adesso va

--- rounding_ITA.py
+++ rounding_ITA.py
@@ -9,21 +9,22 @@ def round_up(n, decimals=0):
 def rounding(number_to_round):
 #    rounded_number = 1
     last_decimal = int(str(number_to_round)[-1])
-    numero_to_round_string = str(number_to_round)
-    if last_decimal == 0 or 1 or 2:
-        numero_to_round_string[-1] = "0"
-    elif last_decimal == 3 or 4 or 5 or 6 or 7:
-        numero_to_round_string[-1] = "5"
-    else:
-        numero_to_round_string[-1] = 0
-        numero = int(numero_to_round_string[-2]) + 1
-        numero_to_round_string[-2] = numero
+    if last_decimal == 0 or last_decimal == 1 or last_decimal == 2:
+        number_to_round = ((number_to_round * 100) - last_decimal) / 100
+    elif last_decimal == 3 or last_decimal == 4 or last_decimal == 5 or last_decimal == 6 or last_decimal == 7:
+        number_to_round = ((number_to_round * 100) + (5 - last_decimal)) / 100
 
-    return float(numero_to_round_string)
+    else:
+        number_to_round = ((number_to_round * 100) + (10 - last_decimal)) / 100
+
+    return number_to_round
 
 
 num = 10.362
 # print(num)
 # print(round_up(num, 2))
+print(rounding(10.33))
 print(rounding(10.37))
+print(rounding(10.39))
+print(rounding(10.32))
 
