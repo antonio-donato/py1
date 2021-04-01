168fabd4fcd9fe8bbc9e5439d9be94dc86437134
Antonio Donato <antonio.donato@cgm.com>
2021-04-01 08:50:05 +0200

    nuovi test e 'or' ristretta'

--- rounding_ITA.py
+++ rounding_ITA.py
@@ -9,22 +9,20 @@ def round_up(n, decimals=0):
 def rounding(number_to_round):
 #    rounded_number = 1
     last_decimal = int(str(number_to_round)[-1])
-    if last_decimal == 0 or last_decimal == 1 or last_decimal == 2:
-        number_to_round = ((number_to_round * 100) - last_decimal) / 100
-    elif last_decimal == 3 or last_decimal == 4 or last_decimal == 5 or last_decimal == 6 or last_decimal == 7:
-        number_to_round = ((number_to_round * 100) + (5 - last_decimal)) / 100
 
+    if last_decimal in {0, 1, 2}:
+        number_to_round = ((number_to_round * 100) - last_decimal) / 100
+    elif last_decimal in {3, 4, 5, 6, 7}:
+        number_to_round = ((number_to_round * 100) + (5 - last_decimal)) / 100
     else:
         number_to_round = ((number_to_round * 100) + (10 - last_decimal)) / 100
 
     return number_to_round
 
 
-num = 10.362
-# print(num)
-# print(round_up(num, 2))
-print(rounding(10.33))
-print(rounding(10.37))
-print(rounding(10.39))
-print(rounding(10.32))
+# num = 10.362
+# print(rounding(10.33))
+# print(rounding(10.37))
+# print(rounding(10.39))
+# print(rounding(10.32))
 
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import asta
 from unittest.mock import patch
 import unittest
+import rounding_ITA
 
 
 class TestAll(unittest.TestCase):
@@ -20,6 +21,14 @@ class TestAll(unittest.TestCase):
     def test_duplicati_email_in_lista(self):
         self.assertTrue(asta.search_email_in_list([["donant@gmail.com", 10], ["donant@outlook.com", 20]], "donant@gmail.com"))
 
+    def test_arrotonda1(self):
+        self.assertTrue((rounding_ITA.rounding(10.37) == 10.35))
+
+    def test_arrotonda2(self):
+        self.assertTrue((rounding_ITA.rounding(10.31) == 10.3))
+
+    def test_arrotonda3(self):
+        self.assertTrue((rounding_ITA.rounding(10.39) == 10.4))
 
 if __name__ == '__main__':
     unittest.main()
